1efbf2692ff53ad5881d0b2b20a6476737fd1dfe
Rosie Keszek <rosiekeszek@hotmail.com>
2022-05-31 21:52:41 -0700

    Add README.md app.js index.html styles.css

---
+++ app.js
@@ -0,0 +1,84 @@
+//let requestFortune = document.querySelector(".button1");
+
+let deliverFortune = document.querySelector(".fortune-display")
+
+let randomNumber = Math.floor(Math.random * 24);
+
+switch (randomNumber) {
+    case 0:
+        console.log('Be careful who you trust. Salt and sugar look the same.');
+        break;
+    case 1:
+        console.log('Ignore previous message.');
+        break;
+    case 2:
+        console.log('There is no angry way to say bubbles.');
+        break;
+    case 3:
+        console.log('It could be better but it\'s good enough.');
+        break;
+    case 4:
+        console.log('The road to success is always under construction.');
+        break;
+    case 5:
+        console.log('You must expect great things of yourself before you can do them.');
+        break;
+    case 6:
+        console.log('You can\'t know, you can only believe. Or not. -C.S. Lewis');
+        break;
+    case 7:
+        console.log('Perfect is boring. Human is beautiful.');
+        break;
+    case 8:
+        console.log('Forever is a long time.');
+        break;
+    case 9:
+        console.log('A party without cake is just a meeting. -Julia Child');
+        break;
+    case 10:
+        console.log('The challenge is not to be perfect. It\'s to be whole. -Jane Fonda');
+        break;
+    case 11:
+        console.log('Nobody gets to tell you how big your dreams can be.');
+        break;
+    case 12:
+        console.log('There is no death. Only a change of worlds.');
+        break;
+    case 13:
+        console.log('A person who never made a mistake never tried anything new.');
+        break;
+    case 14:
+        console.log('Stay close to people who feel like sunlight. -Xan Oku');
+        break;
+    case 15:
+        console.log('Progress not perfection.');
+        break;
+    case 16:
+        console.log('Don\'t make a wish. Make a decision.');
+        break;
+    case 17:
+        console.log('Adjust your eyes. See the same things differently.');
+        break;
+    case 18:
+        console.log('Take nothing personally.');
+        break;
+    case 19:
+        console.log('It\'s good to be prepared but stay open to surprises.');
+        break;
+    case 20:
+        console.log('Let people discover you.');
+        break;
+    case 21:
+        console.log('Be passionate and totally worth the chaos.');
+        break;
+    case 22:
+        console.log('Go outside and play.');
+        break;
+    case 23:
+        console.log('Beware the grand gesture.');
+        break;
+    case 24:
+        console.log('A closed mouth gathers no feet.');
+        break;
+}
+
